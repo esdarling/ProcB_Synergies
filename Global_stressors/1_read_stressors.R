57d2a907de6d2c4bd731e726da1bdbf6236a6d13
cbrown5 <christo.j.brown@gmail.com>
2015-10-01 13:22:00 -0700

    add code

---
+++ Global_stressors/1_read_stressors.R
@@ -0,0 +1,81 @@
+# Load Halpern's (2015 Nature comms) global pressures maps and find out how many stressors are in each grid cell
+# Original data from here -  https://knb.ecoinformatics.org/#view/doi:10.5063/F19Z92TW
+# Paper: Halpern, Benjamin S., et al. "Spatial and temporal changes in cumulative human impacts on the world/'s ocean." Nature communications 6 (2015).
+
+
+#CJ Brown Oct 2015
+# In support of Cote, Darling, Brown in prep. 
+
+rm(list = ls())
+
+library(raster)
+
+setwd('/Users/s2973410/Databases/Global stressors/urn_uuid_975e3a96_c912_4e41_a888_7cccab216bf6/data/')
+
+foldnams <- list.files()
+nfold <- length(foldnams)
+
+pquant <- 0.1 #threshold for quantile above which we count a stressor
+
+sover <- function(x,y){ return(x+y) }
+
+savenam <- paste('/Users/s2973410/Databases/Global stressors/num_stressors_quant', round(pquant*100), '.grd', sep ='')
+
+# Loop through the pressure files
+
+for (ifold in 1:nfold){
+	fnam <- list.files(foldnams[ifold])
+	print(fnam[1])
+	ifile <- grep('.tif', fnam)
+	r <- raster(paste(foldnams[ifold],fnam[ifile[1]], sep ='/'))
+	
+	#Create base raster that will store all values
+	if (ifold ==1){
+		rsum <- raster(r)
+		rsum[] <- 0
+		} 
+	
+	quant <- quantile(r, probs = pquant, type = 7,names = FALSE)
+	
+	# Reclassify values as 0 if below quantile and 1 otherwise
+	rmax <- r@data@max
+	rmin <- r@data@min
+	rcl <- matrix(c(rmin, quant[1], 0, quant[1], rmax, 1), nrow = 2, byrow = T)
+	r2 <- reclassify(r, rcl)
+	rm(r)
+	
+	#Add values to new raster
+	# rsum2 <- rsum + r2
+	rsum <- overlay(rsum, r2, fun = sover)
+	
+	# Manually delete temporary files for r2 (the reclassification, each one is >2 gb, so needs to be removed unless you have a lot of harddrive space)
+	fdel <- filename(r2)
+	fdel2 <- strsplit(fdel, split = '[.]')
+	file.remove(fdel)
+	file.remove(paste(fdel2[[1]][1], '.gri', sep =''))
+	rm(r2)
+	
+	#Get temporary file name for rsum to delete on next iteration
+	if(ifold > 1){ #delete old file
+		fdel2 <- strsplit(fdelrsum, split = '[.]')
+		file.remove(fdelrsum)
+		file.remove(paste(fdel2[[1]][1], '.gri', sep =''))
+		}
+		
+	#Update file name for deleting on next iteration
+	fdelrsum <- filename(rsum)
+
+	gc()
+	
+	}
+
+
+#Plot to check it
+plot(rsum, maxpixels = 10000)
+
+#
+# Save to file
+#
+
+
+ writeRaster(rsum, savenam, overwrite = T)
---
+++ Global_stressors/2_map_stressors.R
@@ -0,0 +1,112 @@
+# Make the map of the number of stressor interactions
+# Using number of pressures raster created in 'read_stressors.R'
+# CJ Brown, E Darling, I Cote. 30 Sept 2015
+
+
+rm(list = ls())
+
+library(raster)
+library(RColorBrewer)
+library(maps)
+
+#
+# Function to make (more) continuous palettes from categories of colours
+#
+
+pal.cont <- function(pal, n){
+	pal <- colorRampPalette(pal)
+	pal(n)
+	}
+
+#
+# Load data
+#
+
+setwd('/Users/s2973410/Databases/Global stressors/')
+rsum.load<- raster('num_stressors_quant10.grd')
+
+#
+#Resample to speed things up for test runs
+#
+rsum <- sampleRegular(rsum.load, size = 1000000, asRaster = T)
+rsum[rsum[]==0] <- NA #get rid of NAs
+
+#Max number of stressors
+maxn <- rsum@data@max
+
+# Choose can determine how many combinates. 
+# First param is number of classes to choose from
+# Second param is number of choices. E.g. following plots number of combinations of maxn numbers taking from 1 to maxn  numbers
+# plot(1:maxn, cumsum(choose(maxn, 1:maxn)))
+
+#Number of possible interactions starting at one way (ie none), then 2 way interactions etc...
+nints_2way <- c(0, choose(1:maxn, 2))
+nints_3way <- c(0, choose(1:maxn, 3))
+nints_2_3way <- nints_2way + nints_3way
+
+rcl <- cbind(0:maxn, nints_2_3way)
+#This step takes about a 90 seconds if using full dataset
+system.time(rints <- reclassify(rsum, rcl))
+
+maxint <- max(nints_2_3way)
+#
+# Figure params
+#
+colNA <- 'white'
+
+#Number stressors plot
+colsum <- pal.cont(brewer.pal(8, 'BuPu'), maxn)
+numbreaks <- c(0,1:maxn)
+
+#Number interactions plot
+colint <- pal.cont(brewer.pal(8, 'Reds'), 20)
+numbreaks.int <- round(c(0,seq(1, 1000, length.out = 20)))
+
+
+#lines plot
+lwduse <- 2
+collines <- c('black','orange', 'red')
+
+#
+# Set up and plot the figure
+#
+dev.new(width = 11, height = 3)
+par(mfrow = c(1,2), mar = c(2,2,1,6))
+
+# Will need to use layout and 'legend.only = T' to position legends
+
+#Part 1, number of stressors
+plot(rsum, maxpixels = 10000, colNA = colNA, col = colsum, breaks = numbreaks, lab.breaks = NA, xaxt = 'n', yaxt = 'n', axis.args = list(at = c(1, 5, 10, 15), labels = T), legend.width = 1.5, asp = NA, bty = 'n')
+
+#Part 2, number of interactions
+plot(rints, maxpixels = 10000, colNA = colNA, col = colint, breaks = numbreaks.int, lab.breaks = NA, xaxt = 'n', yaxt = 'n', axis.args = list(at = c(1, 500, 1000), labels = T), legend.width = 1.5, asp = NA, bty = 'n')
+
+
+#Part 3, subset, relationship num stressors to num interactions
+plotdim <- par("plt")
+xleft <- plotdim[2]*0.69
+xright <- plotdim[2]*0.8
+ybottom <- plotdim[3]
+ytop <- plotdim[4]*0.4
+
+# set position for inset
+par(
+  fig = c(xleft, xright, ybottom, ytop)
+  , mar=c(0,0,0,0)
+  , new=TRUE
+  )
+
+# add inset
+par(las = 1)
+plot(rcl, xlab = 'Number of stressors', ylab = 'Number of interactions', type = 'l', lwd = lwduse, col = collines[1], bg = 'white', xaxp = c(0, maxn, 1), yaxp = c(0, maxint, 1), cex.axis = 0.8)
+rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = 
+"white")
+lines(rcl, lwd = lwduse, col = collines[1])
+lines(rcl[,1], nints_2way, lwd = lwduse, col = collines[2])
+lines(rcl[,1], nints_3way, lwd = lwduse, col = collines[3])
+
+legend('topleft', legend = c('2 and 3-way','2-way', '3-way'), lwd = 2, col = collines, cex = 0.5, bty = 'n')
+
+
+
+
